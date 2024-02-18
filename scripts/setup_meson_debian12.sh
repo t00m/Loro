ee4cb6ce0f59acf8343bccdb8ba7102172755fc4
Tomás Vírseda <tomasvirseda@gmail.com>
2024-02-18 21:55:09 +0100

    chore: update scripts to setup this project in Debian 12

--- scripts/setup_meson_debian12.sh
+++ scripts/setup_meson_debian12.sh
@@ -1,1 +1,5 @@
-sudo apt install meson ninja-build blueprint-compiler libglib2.0-dev gettext
+sudo apt install meson ninja-build blueprint-compiler libglib2.0-dev gettext python3-pip
+# pip3 install -U spacy --user --break-system-packages
+pip3 install -U $(spacy info de_core_news_sm --url) --user --break-system-packages
+pip3 install -U $(spacy info de_core_news_lg --url) --user --break-system-packages
+
