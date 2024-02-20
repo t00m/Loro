c1c6061896dffa89040636433e85d377c782d16e
t00m <tomasvirseda@gmail.com>
2024-02-20 22:52:41 +0100

    chore: add python3-rich dependency to setup_meson_debian12.sh

--- scripts/setup_meson_debian12.sh
+++ scripts/setup_meson_debian12.sh
@@ -1,4 +1,4 @@
-sudo apt install meson ninja-build blueprint-compiler libglib2.0-dev gettext python3-pip python3-openpyxl
+sudo apt install meson ninja-build blueprint-compiler libglib2.0-dev gettext python3-pip python3-openpyxl python3-rich
 # pip3 install -U spacy --user --break-system-packages
 pip3 install -U $(spacy info de_core_news_sm --url) --user --break-system-packages
 pip3 install -U $(spacy info de_core_news_lg --url) --user --break-system-packages
