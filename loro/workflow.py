30110eb29a0cf4bfcd8c290a87edf2bd929d4b0e
Tomás Vírseda <tomasvirseda@gmail.com>
2024-02-23 17:33:02 +0100

    refactor(Workflow): Skip files whose detected language differs from source language specified

--- loro/workflow.py
+++ loro/workflow.py
@@ -11,48 +11,61 @@ from loro.extractors import whatsapp
 from loro.services.nlp.spacy import tokenize_sentence
 from loro.services.nlp.spacy import get_glossary_term_explained
 from loro.services.nlp.spacy import load_model
+from loro.services.nlp.spacy import detect_language
 from loro.core.util import is_valid_word
 from loro.core.util import get_metadata_from_filepath
 from loro.core.util import get_hash
 from loro.core.log import get_logger
 from loro import dictionary
 from loro.dictionary import Dictionary
+from loro.core.env import ENV
 
 class Workflow:
-    def __init__(self, model_name: str):
+    def __init__(self):
         self.log = get_logger('Workflow')
-        load_model(model_name)
-        self.log.info("Model '%s' loaded", model_name)
+        self.source, self.target = ENV['Projects']['Default']['Languages']
+        self.model_type = ENV['Languages'][self.source]['model']['default']
+        self.model_name = ENV['Languages'][self.source]['model'][self.model_type]
+        self.log.info("Source language: '%s'", self.source)
+        self.log.info("Target language: '%s'", self.target)
+        self.log.info("Loading model '%s' for language '%s'", self.model_name, self.source)
+        load_model(self.model_name)
+        self.log.info("Model '%s' loaded", self.model_name)
         self.dictionary = Dictionary()
         self.progress = None
 
     def __del__(self):
         pass
-      # ~ self.log.info("Workflow class destroyed")
 
     def start(self, filepath: str):
-        # ~ self.log.info("Processing input file '%s'", os.path.basename(filepath))
-        with Progress() as self.progress:
-            topic, subtopic = get_metadata_from_filepath(filepath)
-            sentences = open(filepath, 'r').readlines()
-            task = self.progress.add_task("[green]%s..." % os.path.basename(filepath), total=len(sentences))
-            workbook = self.process_input(sentences, task)
-            # ~ self.log.info("\tUseful sentences: %d", len(workbook.keys()))
-            self.process_workbook(topic, subtopic, workbook)
-            # ~ self.log.info("End processing input file '%s'", os.path.basename(filepath))
-            # ~ self.log.info("Topics: %s\nSubtopics: %s", ', '.join(self.dictionary.get_topics()), ', '.join(self.dictionary.get_subtopics()))
+        result = detect_language(open(filepath).read())
+        lang = result['language']
+        score = int((result['score']*100))
+        if lang == self.source and score > 85:
+            with Progress() as self.progress:
+                topic, subtopic = get_metadata_from_filepath(filepath)
+                sentences = open(filepath, 'r').readlines()
+                task = self.progress.add_task("[green]%s..." % os.path.basename(filepath), total=len(sentences))
+                workbook = self.process_input(sentences, task)
+                self.process_workbook(topic, subtopic, workbook)
+        else:
+            self.log.error("File will NOT be processed: '%s'", os.path.basename(filepath))
+            self.log.error("Language '%s' detected with %d%% of confidenciality", lang, score)
+            if lang != self.source:
+                self.log.error("Language detected ('%s') differs from source language ('%s')", lang, self.source)
+            else:
+                self.log.error("Score detection for detected language is less than 85%", score)
 
     def process_input(self, sentences: [], task) -> {}:
         workbook = {}
         jobs = []
         jid = 1
-        # ~ self.log.info("\tGot %d sentences", len(sentences))
         MAX_WORKERS = 40
         with Executor(max_workers=MAX_WORKERS) as exe:
             for sentence in sentences:
                 data = (sentence, jid, task)
                 job = exe.submit(self.process_sentence, data)
-                job.add_done_callback(self.__sentence_processed) # Not needed
+                job.add_done_callback(self.__sentence_processed)
                 jobs.append(job)
                 jid += 1
 
@@ -66,6 +79,15 @@ class Workflow:
                     workbook[sid]['tokens'] = list(metadata['tokens'])
                     workbook[sid]['entities'] = list(metadata['entities'])
         return workbook
+
+    def __sentence_processed(self, future):
+        time.sleep(random.random())
+        cur_thread = threading.current_thread().name
+        x = future.result()
+        jid, metadata, task = x
+        self.progress.advance(task)
+        if cur_thread != x:
+            return x
 
     def process_sentence(self, data: tuple) -> tuple:
         (sentence, jid, task) = data
@@ -108,24 +130,11 @@ class Workflow:
             saved = self.dictionary.add_sentence(sid, workbook[sid]['sentence'])
             if saved:
                 nsents += 1
-
                 # Save tokens, lemmas and pos tags
                 for token in workbook[sid]['tokens']:
                     self.dictionary.add_token(token, sid, workbook)
 
 
-        # ~ self.log.info("\t\tSaved %d sentences", nsents)
-        # ~ self.log.info("\tWorkbook processed")
-
-    def __sentence_processed(self, future):
-        time.sleep(random.random())
-        cur_thread = threading.current_thread().name
-        x = future.result()
-        jid, metadata, task = x
-        self.progress.advance(task)
-        # ~ self.log.info("%d > %s", jid, metadata['sentence'])
-        if cur_thread != x:
-            return x
 
 
 
