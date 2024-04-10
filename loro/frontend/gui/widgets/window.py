14c04318e506d4bff48a7861f7939337909e1440
Tomás Vírseda <tomasvirseda@gmail.com>
2024-04-10 23:07:29 +0200

    refactor(Window): reorg code

--- loro/frontend/gui/widgets/window.py
+++ loro/frontend/gui/widgets/window.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
+import sys
 import time
 import threading
 from gi.repository import Gio, Adw, GObject, Gtk  # type:ignore
@@ -26,61 +27,21 @@ class Window(Adw.ApplicationWindow):
     def __init__(self, **kwargs) -> None:
         self.log = get_logger('Window')
         super().__init__(**kwargs)
-        GObject.GObject.__init__(self)
-        GObject.signal_new('window-presented', Window, GObject.SignalFlags.RUN_LAST, None, () )
-        GObject.signal_new('workbooks-updated', Window, GObject.SignalFlags.RUN_LAST, None, () )
-        self.connect('window-presented', self._on_finish_loading)
+        # ~ GObject.GObject.__init__(self)
+        # ~ GObject.signal_new('window-presented', Window, GObject.SignalFlags.RUN_LAST, None, () )
+        # ~ GObject.signal_new('workbooks-updated', Window, GObject.SignalFlags.RUN_LAST, None, () )
+        # ~ self.connect('window-presented', self._on_finish_loading)
         self.connect('close-request', self._on_close_request)
         self.app = kwargs['application']
         self._create_actions()
         self._build_ui()
         self.present()
-        self.emit('window-presented')
-
-    def _on_close_request(self, *args):
-        self.log.info("Quit application requested by user")
-
-    def _on_finish_loading(self, *args):
-        # ViewSwitcher
-        # Widgets
-        browser = self.app.add_widget('browser', Browser(self.app))
-        # ~ editor = self.app.add_widget('editor', Editor(self.app))
-        dashboard = self.app.add_widget('dashboard', Dashboard(self.app))
-        # ~ translator = self.app.add_widget('translator', Translator(self.app))
-        viewstack = self.app.add_widget('window-viewstack', Adw.ViewStack())
-        viewstack.connect("notify::visible-child", self._on_stack_page_changed)
-        viewstack.add_titled_with_icon(dashboard, 'dashboard', 'Dashboard', 'com.github.t00m.Loro-go-home-symbolic')
-        # ~ viewstack.add_titled_with_icon(editor, 'workbooks', 'Workbooks', 'com.github.t00m.Loro-workbooks')
-        viewstack.add_titled_with_icon(browser, 'study', 'Study', 'com.github.t00m.Loro-study-symbolic')
-        # ~ viewstack.add_titled_with_icon(translator, 'translator', 'Translations', 'com.github.t00m.Loro-translate-symbolic')
-        viewswitcher = self.app.add_widget('viewswitcher', Adw.ViewSwitcher())
-        viewswitcher.set_stack(viewstack)
-        headerbar = self.app.get_widget('headerbar')
-        headerbar.set_title_widget(viewswitcher)
-        mainbox = self.app.get_widget('window-mainbox')
-        mainbox.append(headerbar)
-        mainbox.append(viewstack)
-        self.set_content(mainbox)
-        # ~ dashboard.update_dashboard()
-        self.update_dropdown_workbooks()
-        self.connect('workbooks-updated', self.update_dropdown_workbooks)
-        # ~ editor.connect('workbooks-updated', editor.update_editor)
-        # ~ progressbar = self.app.get_widget('progressbar')
-        # ~ mainbox.append(progressbar)
-
-    def show_stack_page(self, page_name: str):
-        viewstack = self.app.get_widget('window-viewstack')
-        viewstack.set_visible_child_name(page_name)
-
-    def _on_stack_page_changed(self, viewstack, gparam):
-        page = viewstack.get_visible_child_name()
-        # ~ self.log.debug("Switched to page '%s'", page)
+        # ~ self.emit('window-presented')
 
     def _build_ui(self):
         self.set_title(_("Loro"))
         self.app.add_widget('window-mainbox', self.app.factory.create_box_vertical(spacing=0, margin=0, hexpand=True, vexpand=True))
         headerbar = self.app.add_widget('headerbar', Adw.HeaderBar())
-
         # Menu
         menu: Gio.Menu = Gio.Menu.new()
 
@@ -107,34 +68,55 @@ class Window(Adw.ApplicationWindow):
         ddWorkbooks = self.app.factory.create_dropdown_generic(Workbook, enable_search=True)
         self.app.add_widget('dropdown-workbooks', ddWorkbooks)
         ddWorkbooks.set_valign(Gtk.Align.CENTER)
-        # ~ ddWorkbooks.connect("notify::selected-item", self._on_workbook_selected)
-
         ddWorkbooks.set_hexpand(False)
         boxDpdWorkbooks.append(ddWorkbooks)
         headerbar.pack_start(boxDpdWorkbooks)
-        # ~ progressbar = self.app.add_widget('progressbar', Gtk.ProgressBar())
-        # ~ progressbar.set_hexpand(True)
-        # ~ progressbar.set_valign(Gtk.Align.CENTER)
-        # ~ progressbar.set_show_text(True)
 
-        # ~ headerbar.pack_end(progressbar)
-        # ~ self.props.width_request = 1024
-        # ~ self.props.height_request = 768
+        browser = self.app.add_widget('browser', Browser(self.app))
+        dashboard = self.app.add_widget('dashboard', Dashboard(self.app))
+        viewstack = self.app.add_widget('window-viewstack', Adw.ViewStack())
+        viewstack.connect("notify::visible-child", self._on_stack_page_changed)
+        viewstack.add_titled_with_icon(dashboard, 'dashboard', 'Dashboard', 'com.github.t00m.Loro-go-home-symbolic')
+        viewstack.add_titled_with_icon(browser, 'study', 'Study', 'com.github.t00m.Loro-study-symbolic')
+        viewswitcher = self.app.add_widget('viewswitcher', Adw.ViewSwitcher())
+        viewswitcher.set_stack(viewstack)
+        headerbar = self.app.get_widget('headerbar')
+        headerbar.set_title_widget(viewswitcher)
+        mainbox = self.app.get_widget('window-mainbox')
+        mainbox.append(headerbar)
+        mainbox.append(viewstack)
+        self.set_content(mainbox)
+        self.app.actions.update_dropdown_workbooks()
 
-    # ~ def _on_workbook_selected(self, dropdown, gparam):
-        # ~ dashboard = self.app.get_widget('dashboard')
-        # ~ editor = self.app.get_widget('editor')
-        # ~ translator = self.app.get_widget('translator')
-        # ~ workbook = dropdown.get_selected_item()
-        # ~ if workbook is None:
-            # ~ return
+    def _on_close_request(self, *args):
+        self.log.info("Quit application requested by user")
+        self.app.quit()
+        sys.exit(0)
 
-        # ~ if workbook.id is None:
-            # ~ viewstack = self.app.get_widget('dashboard-viewstack')
-            # ~ viewstack.set_visible_child_name('wb-none')
-        # ~ else:
-            # ~ dashboard.set_current_workbook(workbook)
-            # ~ self.app.actions.report_display()
+    def _on_finish_loading(self, *args):
+        browser = self.app.add_widget('browser', Browser(self.app))
+        dashboard = self.app.add_widget('dashboard', Dashboard(self.app))
+        viewstack = self.app.add_widget('window-viewstack', Adw.ViewStack())
+        viewstack.connect("notify::visible-child", self._on_stack_page_changed)
+        viewstack.add_titled_with_icon(dashboard, 'dashboard', 'Dashboard', 'com.github.t00m.Loro-go-home-symbolic')
+        viewstack.add_titled_with_icon(browser, 'study', 'Study', 'com.github.t00m.Loro-study-symbolic')
+        viewswitcher = self.app.add_widget('viewswitcher', Adw.ViewSwitcher())
+        viewswitcher.set_stack(viewstack)
+        headerbar = self.app.get_widget('headerbar')
+        headerbar.set_title_widget(viewswitcher)
+        mainbox = self.app.get_widget('window-mainbox')
+        mainbox.append(headerbar)
+        mainbox.append(viewstack)
+        self.set_content(mainbox)
+        self.app.actions.update_dropdown_workbooks()
+
+
+    def show_stack_page(self, page_name: str):
+        viewstack = self.app.get_widget('window-viewstack')
+        viewstack.set_visible_child_name(page_name)
+
+    def _on_stack_page_changed(self, viewstack, gparam):
+        page = viewstack.get_visible_child_name()
 
     def _create_actions(self) -> None:
         """Create actions for main menu"""
@@ -185,18 +167,3 @@ class Window(Adw.ApplicationWindow):
             lambda *_: self.props.application.quit(),
             ["<primary>q", "<primary>w"],
         )
-
-    def update_dropdown_workbooks(self, *args):
-        workbooks = self.app.workbooks.get_all()
-        ddWorkbooks = self.app.get_widget('dropdown-workbooks')
-        data = []
-        wbnames = workbooks.keys()
-        if len(wbnames) == 0:
-            ddWorkbooks.set_visible(False)
-            data.append((None, 'No workbooks available'))
-        else:
-            ddWorkbooks.set_visible(True)
-            for workbook in wbnames:
-                data.append((workbook, "Workbook %s" % workbook))
-        self.app.actions.dropdown_populate(ddWorkbooks, Workbook, data)
-
