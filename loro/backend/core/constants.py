abf46264230423a1682a4e54e4d6b77326d7465d
Tomás Vírseda <tomasvirseda@gmail.com>
2024-04-22 21:25:44 +0200

    feat(Core): Add backup dir as a constant string

--- loro/backend/core/constants.py
+++ loro/backend/core/constants.py
@@ -4,11 +4,11 @@
 import os
 import subprocess
 
-
 def get_user_documents_dir():
     return subprocess.check_output(["xdg-user-dir", "DOCUMENTS"], universal_newlines=True).strip()
 
 LORO_USER_DIR = os.path.join(get_user_documents_dir(), 'Loro')
 LORO_USER_PROJECTS_DIR = os.path.join(get_user_documents_dir(), 'Loro', 'Projects')
+LORO_USER_BACKUP_DIR = os.path.join(get_user_documents_dir(), 'Loro', 'Backup')
 LORO_USER_CONFIG_DIR = os.path.join(get_user_documents_dir(), 'Loro', '.config')
 LORO_USER_CNF = os.path.join(LORO_USER_CONFIG_DIR, 'loro.conf')
